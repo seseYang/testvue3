0fe91e9a6f9564e330069aea438df35e80a93bfb
seseYang <59229799+seseYang@users.noreply.github.com>
2020-02-08 17:20:19 +0800

    腾讯新闻移动端视图

---
+++ src/components/Lgoin.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <van-field
+                v-model="code"
+                clearable
+                autofocus
+                label="QQ号/手机号/邮箱"
+                left-icon="music-o"
+                placeholder="显示清除图标"
+        />
+        <van-field
+                clearable
+                label="文本"
+                v-model="password"
+                type="password"
+                placeholder="显示清除图标"
+        />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Lgoin"
+        ,data(){
+            return{
+                code:""
+                ,password:""
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
